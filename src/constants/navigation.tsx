cb0e68efd9fc0aafc705c2404960b0de54abc0ef
Bot-Rakshit <singhrakshit003@gmail.com>
2025-01-01 20:40:39 +0530

    refactor: consolidate navigation files and update social links

--- src/constants/navigation.tsx
+++ src/constants/navigation.tsx
@@ -5,21 +5,21 @@ export const navigationItems: NavigationItem[] = [
   {
     title: "ChessWrapped",
     icon: <IconHome className="h-full w-full text-white stroke-[1.5]" />,
-    href: "#",
+    href: "/",
   },
   {
     title: "About",
     icon: <IconInfoCircle className="h-full w-full text-white stroke-[1.5]" />,
-    href: "#about",
+    href: "/about",
   },
   {
     title: "GitHub",
     icon: <IconBrandGithub className="h-full w-full text-white stroke-[1.5]" />,
-    href: "https://github.com/rakshit087/chesswrapped",
+    href: "https://github.com/bot-rakshit",
   },
   {
     title: "Twitter",
     icon: <IconBrandTwitter className="h-full w-full text-white stroke-[1.5]" />,
-    href: "https://twitter.com/rakshit087",
+    href: "https://twitter.com/Ra1kshit",
   },
 ]; 
